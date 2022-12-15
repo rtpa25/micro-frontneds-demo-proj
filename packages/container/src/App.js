4765168a202c5113f22e4be81935c8581ec0b0b1
Ronit Panda <pandaronit25@gmail.com>
2022-12-15 17:58:49 +0530

    feat: fixed prod styling collisions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <MarketingApp />;
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <Header />
+                <MarketingApp />;
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
 
