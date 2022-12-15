0637ecde49bf7ffc4107be973533cf572d5ca29f
Ronit Panda <pandaronit25@gmail.com>
2022-12-15 12:58:45 +0530

    feat: updated the way we export the app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
-export default () => {
+const App = () => {
     return <MarketingApp />;
 };
+
+export default App;
