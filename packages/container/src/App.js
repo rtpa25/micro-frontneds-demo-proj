ae7885bef1a372feb7fb38f8031e4a9ed4e542ef
Ronit Panda <pandaronit25@gmail.com>
2022-12-15 17:13:41 +0530

    feat(container): added a shared header to the app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
     return (
-        <div>
+        <BrowserRouter>
+            <Header />
             <MarketingApp />;
-        </div>
+        </BrowserRouter>
     );
 };
 
